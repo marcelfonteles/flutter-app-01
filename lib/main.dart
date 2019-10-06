26d420c39083db8ef73de6e68db6e02a242c5421
marcelfonteles <marcelfonteles@gmail.com>
2019-10-05 22:35:59 -0300

    learning about flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // Some code here...
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var index2 = 0;
+  var questions = ["Whats is your name?", "What\'s is your favorite color?"];
+
+  void answerQuestion() {
+    setState(() {
+      index2 += 1;
+    });
+    print(index2);
+  }
+
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
@@ -14,18 +32,28 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Column(
-          children: [
-            Text("The Question?"),
-            Row(
-              children: [
-                RaisedButton(child: Text("Answer 1")),
-                RaisedButton(child: Text("Answer 2")),
-                RaisedButton(child: Text("Answer 3")),
-              ]
-            )
-          ]
-        ),
+        body: Column(children: [
+          Row(children: [
+            Text(questions.elementAt(index2)),
+          ]),
+          Row(children: [
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Anwser 2"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                // Do some stuff..
+                print("Anwser 3.");
+              },
+            ),
+          ]),
+        ]),
       ),
     );
   }
